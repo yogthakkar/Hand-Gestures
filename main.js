6e03bba526ab61a510875a97d592c11254de5798
yogthakkar <97528440+yogthakkar@users.noreply.github.com>
2022-08-02 18:46:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'">';
+    });
+}
+
+console.log('ml5 version: ', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/WefHtC3PK/model.json', modelLoaded());
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1 = "First Prediction is " + prediction_1;
+    speak_data_2 = " and Second Prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
